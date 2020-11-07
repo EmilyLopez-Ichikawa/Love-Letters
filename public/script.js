c938798f3d2538c2788638c386b7b276fd9eb194
EmilyLopez-Ichikawa <e.lopez-ichikawa@wustl.edu>
2020-11-07 00:57:00 -0500

    Create boxes with all fields from each entry, moved data from airtable to firestore

--- public/script.js
+++ public/script.js
@@ -1,37 +1,88 @@
-// window.onload = function() {
-// 	fetch('https://api.airtable.com/v0/apppWQcOe5ZLgfcpZ/Table%201?api_key=keyf2wfsXxVRjlcsT')
+const collection = firebase.firestore().collection('stories');
+
+// fetch('https://api.airtable.com/v0/apppWQcOe5ZLgfcpZ/Table%201?api_key=keyf2wfsXxVRjlcsT')
 // 	.then(response => response.json())
-// 	.then(function(data) {
-// 		console.log(data);
+// 	.then(function (data) {
+// 		// console.log(data);
 // 		let records = data.records;
-// 		for(let i = 0; i < records.length; i++) {
-// 			var entry = document.createElement("div");
-// 			var text = document.createTextNode(records[i].fields.Description);
-// 			entry.appendChild(text);
-// 			entry.setAttribute("class", "story")
 
-// 			var attachments = records[i].fields.Attachments
-// 			if(attachments) {
-// 				for(var j = 0; j < attachments.length; j++) {
-// 					var img = document.createElement("img");
-// 					img.setAttribute('src', records[i].fields.Attachments[j].url);
-// 					entry.appendChild(img);
-// 				}
-// 			}
+// 		for (var i = 0; i < records.length; i++) {
 
-// 			document.getElementById("stories").appendChild(entry);
-// 		}
+// 			collection.add(records[i].fields)
+// 			.then(function (docRef) {
+// 				console.log("Document written with ID: ", docRef.id);
+// 			})
+// 			.catch(function (error) {
+// 				console.error("Error adding document: ", error);
+// 			});
+
+// 		}	
+
 // 	});
-// }
 
-const db = firebase.firestore();
 
-// Reference the document
-const myPost = db.collection('stories').doc('cYaDbo2IOGUIEP5A4X5X');
 
-// Listen to realtime changes 
-myPost.onSnapshot(doc => {
 
-	const data = doc.data();
-	console.log(data);
-})
+async function getStories() {
+	const snapshot = await collection.get();
+	return snapshot.docs.map(doc => doc.data());
+}
+
+getStories().then(function (stories) {
+	for (let i = 0; i < stories.length; i++) {
+		var dbEntry = stories[i];
+
+		var newDiv = document.createElement("div");
+
+		for (const [key, value] of Object.entries(stories[i])) {
+			if(key != "Attachments"){
+				var text = document.createTextNode(value);
+				var label = document.createElement("h3");
+				label.appendChild(document.createTextNode(key));
+				newDiv.appendChild(label);
+				newDiv.appendChild(text);
+				
+			} else {
+				var attachments = dbEntry.Attachments
+				var label = document.createElement("h3");
+				label.appendChild(document.createTextNode("Attachments"));
+				newDiv.appendChild(label);
+				for (var j = 0; j < attachments.length; j++) {
+					var img = document.createElement("img");
+					img.setAttribute('src', attachments[j].url);
+					newDiv.appendChild(img);
+				}
+			}
+		}
+		newDiv.setAttribute("class", "story");
+		// var name = document.createTextNode(dbEntry.Name);
+
+		// var header = document.createElement("h3");
+		// header.appendChild(document.createTextNode("Name"))
+		// newDiv.appendChild(header);
+		// newDiv.appendChild(name);
+
+		// var name = document.createTextNode(dbEntry.Email);
+		// newDiv.appendChild(document.createElement("h2").appendChild(document.createTextNode("Email")));
+		// newDiv.appendChild(name);
+
+		// var text = document.createTextNode(dbEntry.Description);
+		// newDiv.appendChild(document.createElement("h2").appendChild(document.createTextNode("Story")));
+		// newDiv.appendChild(text);
+		// newDiv.setAttribute("class", "story")
+
+		// var attachments = dbEntry.Attachments
+		// if (attachments) {
+		// 	for (var j = 0; j < attachments.length; j++) {
+		// 		var img = document.createElement("img");
+		// 		img.setAttribute('src', dbEntry.Attachments[j].url);
+		// 		newDiv.appendChild(img);
+		// 	}
+		// }
+
+		document.getElementById("stories").appendChild(newDiv);
+	}
+});
+
+
+
